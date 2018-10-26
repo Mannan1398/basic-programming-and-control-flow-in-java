dca225d9a819d13c6fe02c1b7ed8a5a73e624e2c
Mannan1398 <42761505+Mannan1398@users.noreply.github.com>
2018-10-26 10:49:50 +0530

    Create basic"*" pattern.java

---
+++ basic"*" pattern.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+class Main
+{
+public static void main(String[] x)
+{
+Scanner i=new Scanner(System.in);
+int a;
+int b;
+System.out.println("input n");
+int n;
+n=i.nextInt();
+for(a=0;a<n;a++)
+{
+for(b=0;b<a;b++)
+{
+System.out.print("*");
+}
+System.out.print("\n");
+
+}
+}
+
+}
